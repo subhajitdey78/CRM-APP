3e6c7932c43991654fce3b08fc98869a6597aa56
subhajitdey78 <107293843+subhajitdey78@users.noreply.github.com>
2023-02-02 12:06:38 +0530

    First commit

---
+++ 22. CRM Frontend - Admin(1st Feb 2023 - 6th Feb 2023)/crm-frontend/src/componenets/Sidebar.js
@@ -0,0 +1,1 @@
+import { CSidebar, CSidebarNav, } 
---
+++ 22. CRM Frontend - Admin(1st Feb 2023 - 6th Feb 2023)/crm-frontend/src/pages/Admin.js
@@ -0,0 +1,19 @@
+function Admin() {
+    return (
+        <div className= "row bg-light">
+            <div className = "col-1"><Sidebar home="/"/></div>
+            <div className ="col my-4 vh-100">
+                <div className= "Container">
+                    <div>
+                        {/*Main Admin DashBoard*/}
+                        <h3>Welcome,</h3>
+                        <p>Take a quick look at you admin starts below,</p>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Admin;
