b4931d61bac71caf21ec87bebf1d1995396fd4eb
subhajitdey78 <107293843+subhajitdey78@users.noreply.github.com>
2023-02-11 11:37:29 +0530

    First commit

--- 23. CRM Frontend - Engineer(8th Feb 2023 - 13th Feb 2023)/crm-frontend/src/pages/Engineer.js
+++ 23. CRM Frontend - Engineer(8th Feb 2023 - 13th Feb 2023)/crm-frontend/src/pages/Engineer.js
@@ -1,7 +1,17 @@
-import React from "react";
+import React, {useState} from "react";
+import Sidebar from "../componenets/Sidebar"
+
 function Enginner() {
+    const [ticketUpdateModal, setTicketUpadteModal] = useState(false)
     return (
-        <h2>ENGINEER DASHBORAD</h2>
+        <div className="bg-light">
+            <div className="col-1"><Sidebar home= '/'/></div>
+            <div className="container vh-100">
+                <h3 className="text-primary text-center">Welcome {localStorage.name}</h3>
+                <p className="text-muted text-center">Take a  look at your dashboard.</p>
+
+            </div>
+        </div>
     );
 }
 
