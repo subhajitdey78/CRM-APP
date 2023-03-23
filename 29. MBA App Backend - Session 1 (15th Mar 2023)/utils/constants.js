acfeebd456bac89a229952a900703dead44a7109
subhajitdey78 <107293843+subhajitdey78@users.noreply.github.com>
2023-03-23 16:47:06 +0530

    First commit

---
+++ 29. MBA App Backend - Session 1 (15th Mar 2023)/utils/constants.js
@@ -0,0 +1,10 @@
+/**
+ * This will contain the constants names used through out the code
+ */
+module.exports = {
+    releaseStatus: {
+        unreleased: 'UNRELEASED',
+        released: 'RELEASED',
+        blocked: 'BLOCKED'
+    }
+}
