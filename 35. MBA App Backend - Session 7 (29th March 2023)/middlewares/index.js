e89e156264e21cbfa06ee184c1f757be3d24aab0
subhajitdey78 <107293843+subhajitdey78@users.noreply.github.com>
2023-04-03 17:40:16 +0530

    First Commit

--- 35. MBA App Backend - Session 7 (29th March 2023)/middlewares/index.js
+++ 35. MBA App Backend - Session 7 (29th March 2023)/middlewares/index.js
@@ -3,11 +3,12 @@ const verifyTheatreReqBody = require("./validateTheatreReqBody");
 const authJwt = require("./authjwt");
 const verifyUserReqBody = require("./validateUserReqBody");
 const verifyBookingReqBody = require("./verifyBookingReqBody");
-
+const verifyPaymentReqBody = require("./verifyPaymentReqBody");
 module.exports = { 
     verifyMovieReqBody,
     verifyTheatreReqBody,
     authJwt,
     verifyUserReqBody,
-    verifyBookingReqBody 
+    verifyBookingReqBody, 
+    verifyPaymentReqBody
 }
