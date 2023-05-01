236d86e427b566c54a5a1c3b5e655fdcabfda811
subhajitdey78 <107293843+subhajitdey78@users.noreply.github.com>
2023-05-01 15:28:50 +0530

    First Commit

---
+++ 38. MBA App Frontend - Session 2 (19th April 2023)/mba-frontend/src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import AppRouter from '../components/routes/appRoutes';
+
+function App() {
+  return (
+   <AppRouter />
+  );
+}
+
+export default App;
---
+++ 38. MBA App Frontend - Session 2 (19th April 2023)/mba-frontend/src/pages/auth/auth.js
@@ -0,0 +1,5 @@
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { DropDown, DropdownButton } from "react-bootstrap";
+import { signIn, signUp } from "../../api/auth";
+
