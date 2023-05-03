21cabfc1518f0821944a4542bea243dc1d68ab8c
subhajitdey78 <107293843+subhajitdey78@users.noreply.github.com>
2023-05-03 09:35:04 +0530

    First Commit

---
+++ 38. MBA App Frontend - Session 2 (19th April 2023)/mba-frontend/src/components/Navbar/Navbar.js
@@ -0,0 +1,12 @@
+import { CButon } from '@coreui/react'
+import SuggestionInputSearch from 'suggestion-react-input-search'
+
+const Navbar = (props) => {
+    return (
+        <div className='bg-dark px-2'>
+            <div className='row text-center'>
+                
+            </div>
+        </div>
+    )
+}
